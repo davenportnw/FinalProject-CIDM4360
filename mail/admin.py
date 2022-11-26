f159e66aa310ffbcb376f5251e3453af435853b8
davenportnw <davenportnw@gmail.com>
2022-11-25 21:18:39 -0600

    Create django admin

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Resident
+from .models import Package
+
+admin.site.register(Resident)
+admin.site.register(Package)
+from django.contrib import admin
+
 # Register your models here.
