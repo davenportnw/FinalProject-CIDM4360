fabdaf6f618d90c9a2cfae2c2a90f1c38affda77
davenportnw <davenportnw@gmail.com>
2022-11-30 13:24:36 -0600

    Create views and urls

--- mail/views.py
+++ mail/views.py
@@ -5,6 +5,10 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
+def detail(request, resident_id):
+    return HttpResponse("You're looking at resident id %s." % resident_id)
+
+
 from django.shortcuts import render
 
 # Create your views here.
