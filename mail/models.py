0bc3324c194891be40b96578f74338bf61e9acf0
davenportnw <davenportnw@gmail.com>
2022-12-04 19:57:44 -0600

    Update Package model.

--- mail/models.py
+++ mail/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 
 # Create your models here.
+
+
+STATUS_CHOICES = (
+    ("DELIVERED", "delivered"),
+    ("PENDING", "pending"),
+    ("UNKNOWN", "unknown")
+)
 class Package(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     posting_service = models.CharField(max_length=50)
     received = models.DateTimeField('Date office received')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, null=True)
 
 
 class Resident(models.Model):
